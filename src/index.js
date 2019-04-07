91b9034aea5b7eebbb2ef6c7870255ddd410e537
Roy-H <1246799927@qq.com>
2019-04-08 01:46:39 +0800

    qq

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './client/app';
-
+import App from './client/app';
+import './client/style.css';
 
 ReactDOM.render(
-	//<App />,
-	//document.getElementById('root')
+	<App />,
+	document.getElementById('root')
 );
