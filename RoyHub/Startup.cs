4cc046b016913bb548915231c2ed338136d5e52d
Roy-H <1246799927@qq.com>
2019-04-08 14:18:45 +0800

    s

--- RoyHub/Startup.cs
+++ RoyHub/Startup.cs
@@ -20,7 +20,7 @@ namespace RoyHub
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            Console.WriteLine("setup configure");
+            Console.WriteLine("-----setup configure-------");
 
             if (env.IsDevelopment())
             {
