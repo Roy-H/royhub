dfa4f2eb7a020453ef523663302b8163642c31f2
Roy-H <1246799927@qq.com>
2019-05-30 01:53:53 +0800

    update

--- RoyHub/Hubs/ChatHub.cs
+++ RoyHub/Hubs/ChatHub.cs
@@ -152,9 +152,119 @@ namespace RoyHub.Hubs
 
     public class ChatHub : Hub
     {
+        private static ConcurrentDictionary<string, User> ChatClients = new ConcurrentDictionary<string, User>();
+
+        public async Task<List<User>> Login(string name)
+        {
+            if (!ChatClients.ContainsKey(name))
+            {
+                Console.WriteLine($"++ {name} logged in");
+                List<User> users = new List<User>(ChatClients.Values);
+                User newUser = new User { Name = name, ID = Context.ConnectionId };
+                var added = ChatClients.TryAdd(name, newUser);
+                if (!added) return null;
+                //Clients.CallerState.UserName = name;
+
+                //Clients.Others.ParticipantLogin(newUser);
+                await Clients.Others.SendAsync("ParticipantLogin",newUser);
+                return users;
+            }
+            return null;
+        }
+
+        public async Task Logout()
+        {
+            //var name = Clients.CallerState.UserName;
+            var name = "no name";
+            if (!string.IsNullOrEmpty(name))
+            {
+                User client = new User();
+                ChatClients.TryRemove(name, out client);
+                name = client.Name;
+                //Clients.Others.ParticipantLogout(name);
+                await Clients.Others.SendAsync("ParticipantLogout", name);
+                Console.WriteLine($"-- {name} logged out");
+            }
+        }
+
+        public async Task Typing(string recepient)
+        {
+            if (string.IsNullOrEmpty(recepient)) return;
+            //var sender = Clients.CallerState.UserName;
+            var sender = "dd name";
+            User client = new User();
+            ChatClients.TryGetValue(recepient, out client);
+            sender = client.Name;
+            //Clients.Client(client.ID).ParticipantTyping(sender);
+            await Clients.Others.SendAsync("ParticipantTyping", sender);
+        }
+
+        public async Task UnicastTextMessage(string recepient, string message)
+        {
+            //var sender = Clients.CallerState.UserName;
+            var sender = "dd name";
+            if (!string.IsNullOrEmpty(sender) && recepient != sender &&
+                !string.IsNullOrEmpty(message) && ChatClients.ContainsKey(recepient))
+            {
+                User client = new User();
+                ChatClients.TryGetValue(recepient, out client);
+                sender = client.Name;
+                //Clients.Client(client.ID).UnicastTextMessage(sender, message);
+                await Clients.Others.SendAsync("UnicastTextMessage", sender, message);
+            }
+        }
+
+        public async Task BroadcastImageMessage(byte[] img)
+        {
+            //var name = Clients.CallerState.UserName;
+            var name = "no name";
+            if (img != null)
+            {
+                //Clients.Others.BroadcastPictureMessage(name, img);
+                await Clients.Others.SendAsync("BroadcastPictureMessage", name, img);
+            }
+        }
+
+        public async Task UnicastImageMessage(string recepient, byte[] img)
+        {
+            //var sender = Clients.CallerState.UserName;
+            var sender = "dd name";
+            if (!string.IsNullOrEmpty(sender) && recepient != sender &&
+                img != null && ChatClients.ContainsKey(recepient))
+            {
+                User client = new User();
+                ChatClients.TryGetValue(recepient, out client);
+                sender = client.Name;
+                //Clients.Client(client.ID).UnicastPictureMessage(sender, img);
+                await Clients.Others.SendAsync("UnicastPictureMessage", sender, img);
+            }
+        }
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var userName = ChatClients.SingleOrDefault((c) => c.Value.ID == Context.ConnectionId).Key;
+            if (userName != null)
+            {
+                Clients.Others.SendAsync("ParticipantReconnection",userName);
+                Console.WriteLine($"== {userName} reconnected");
+            }
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            var userName = ChatClients.SingleOrDefault((c) => c.Value.ID == Context.ConnectionId).Key;
+            if (userName != null)
+            {
+                Clients.Others.SendAsync("ParticipantDisconnection",userName);
+                Console.WriteLine($"<> {userName} disconnected");
+            }
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
